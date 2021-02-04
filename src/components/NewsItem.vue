b222493895d23bc14c57dc32c4b398c34750d983
sopibonchik <sopibonchik@gmail.com>
2021-02-04 17:41:57 +0300

    ajax request for news implemented

---
+++ src/components/NewsItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-col cols="12" sm="4" app>
+            <v-card>
+              <v-img
+                v-bind:src="image"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="200px"
+              >
+                <v-card-title v-text="title"></v-card-title>
+              </v-img>
+              <v-card-text>
+                <p>
+                  <slot></slot>
+                </p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn plain color="deep-purple" v-bind:href="link" target="_blank">
+                  <v-icon class="mr-2">mdi-link-variant</v-icon> Читать в
+                  источнике
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+</template>
+
+<script>
+export default {
+  name: 'NewsItem',
+  props: ['image','link','title']
+}
+</script>
